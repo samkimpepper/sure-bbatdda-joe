a405fee1e9e43d10e97736a4514b54f661e0e4ac
samkimpepper <62634206+samkimpepper@users.noreply.github.com>
2023-09-21 15:17:57 +0900

    [feat] 모델 설계 초안

--- sure/alarm/models.py
+++ sure/alarm/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+from user.models import User 
+from chat.models import Message
+
+class Alarm(models.Model):
+    message = models.ForeignKey(Message, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
--- sure/user/models.py
+++ sure/user/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 
 from django.contrib.auth.models import AbstractUser
+from django.utils import timezone 
 
 
 # Create your models here.
 
-class User(models.Model):
-    username = models.CharField(max_length=20, unique=True)
+class User(AbstractUser):
+    manner_tmp = models.DecimalField(max_digits=5, decimal_places=2)
+    location = models.CharField(null=True)
+    created_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.username
