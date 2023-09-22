99d3e87eb783e7be97bb34b9d7f0544623a5bff2
jjm0521 <wkdwhdals05@nabver.com>
2023-09-22 17:10:12 +0900

    [feat] 230922 user 장종민 forms.py추가 프론트 연결

--- sure/sure_app/sure_app_views/views_user.py
+++ sure/sure_app/sure_app_views/views_user.py
@@ -1,4 +1,27 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from ..forms import LoginForm
+
+def register(request):
+    return render(request, 'register.html')
 
 def login(request):
-    return render(request, 'login.html')
+    if request.user.is_authenticated:
+        return redirect('dangun_app:main')
+    
+    else:
+        form = LoginForm(data=request.POST or None)
+        if request.method == "POST":
+
+            if form.is_valid():
+                username = form.cleaned_data['username']
+                password = form.cleaned_data['password']
+                user = authenticate(request, username=username, password=password)
+
+                if user is not None:
+                    login(request, user)
+                    return redirect('dangun_app:main')  # 로그인시 메인
+        return render(request, 'login.html', {'form': form})
+    
+def location(request):
+    return render(request, 'location.html')
