8f99b87d71f7b7bb14e4dbb0452216ac3297e73b
samkimpepper <62634206+samkimpepper@users.noreply.github.com>
2023-10-04 17:31:47 +0900

    [modify] 거래 여부에 따라 거래 버튼 다르게 출력

--- sure/sure_app/static/js/trade_modal.js
+++ sure/sure_app/static/js/trade_modal.js
@@ -12,13 +12,7 @@ function toggleModal(_sellerId, _goodsId) {
     console.log(goodsId);
     modal.classList.toggle("show-modal");
 
-    $.ajax({
-        url: '/goods/trade/complete/' + goodsId + '/',
-        type: 'POST',
-        success: function(data) {
-            console.log(data);
-        }
-    });
+
 }
 
 function windowOnClick(event) {
@@ -28,6 +22,18 @@ function windowOnClick(event) {
 }
 
 submitButton.addEventListener('click', function () {
+    $.ajax({
+        url: '/goods/trade/complete/' + goodsId + '/',
+        type: 'POST',
+        success: function(data) {
+            console.log(data);
+        }
+    });
+    const tradeCompleteBtn = document.querySelector('#trade-complete');
+    tradeCompleteBtn.classList.remove("trade-not-complete");
+    tradeCompleteBtn.classList.add("trade-complete");
+    tradeCompleteBtn.innerHTML = '거래완료';
+
     // 채팅에서 아마 쿼리스트링으로 seller, 매물 정보를 보내줘야합니다
     window.location.href = `/goods/trade/review/?goodsId=${goodsId}&sellerId=${sellerId}`;
 });
