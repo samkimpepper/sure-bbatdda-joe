7381dda7c86b5524b6e471a9b2933f07c3b764c3
RoxyYujinKim <yujinkim0707@gmail.com>
2023-10-05 11:22:26 +0900

    [feat] 챗봇 기능 구현

--- sure/sure_app/sure_app_urls/urls_chat.py
+++ sure/sure_app/sure_app_urls/urls_chat.py
@@ -4,6 +4,7 @@ from ..sure_app_views import views_chat
 # Create your views here.(
 urlpatterns = [
     path('', views_chat.chatting, name='chatting'),
+    path('bot', views_chat.bot, name='chat_bot'),
     path('<int:goods_id>/<int:you_id>', views_chat.chatting, name='chatting_and_chat_id'),
     path('messages/<int:chat_id>', views_chat.messages, name='messages'),
 ]
